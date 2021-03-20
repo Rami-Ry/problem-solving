0f80a25d2b6c51fc5c09bbe7905f0bd5751b70ce
rami-k-rayya <rami.k.rayya@gmail.com>
2021-03-20 08:39:07 +0200

    Added : integer sequence division simpler solution

--- solutions/codeforces/A/integer-sequence-division.cpp
+++ solutions/codeforces/A/integer-sequence-division.cpp
@@ -16,47 +16,39 @@ int main () {
      * We should divide the sequence into to sets where the absolute value of their difference is minimum as possible
      * 
      * The sequence is from 1 -> n
-     * n might be even or odd
+     * We know that if n is 4
+     * so we have
+     * 1, 2, 3, 4
      * 
-     * # if n is even
-     * 
-     * let n be 4
-     * 1, 2, 3, 4 => we can divide it into two groups {1 , 4} {2, 3} = 5 - 5 = 0
+     * we can take two sets {1, 4} = 5, {2, 3} = 5
+     * Each two symmetric numbers (like 1, 4) their sum is equal to the next two symmetric number (like 2, 3) and so on you can do that for big values of n
+     * but not always we have a even count that we can take each two symmetric (opposite) numbers
+     * like if n is 6
+     * 1 2 3 4 5 6
+     * {1, 6} {2, 5} but what about 3 and 4
+     * {1, 6} {2, 5} yeilds a zero as a result of their sum difference
+     * so the result should be 1 because | 4 - 3 | = 1
      * 
      * but what if n is 6
-     * 1, 2, 3, 4, 5, 6 =>  we can't divide it into two groups of pairs, so what we should do ?
-     * let's take {1, 6} {2, 5} there sum difference is 0.
-     * So we take all pairs except the middle two will reamin {3, 4}
-     * since all the groups sum difference is zero we can consider the sequence only {3, 4}
-     * and their difference is 1 always because they are a sequence
+     * 1 2 3 4 5 6 7
+     * we can apply the above rule for the whole sequence except 1 and it might be yield 0 or 1 then subtract 1 and get the absolute value
      * 
-     * so we can consider two cases when n is even
-     * n/2 might be even or odd too
-     * if even so the result is 0
-     * if odd the result is 1
+     * We can approach the above solution
+     * using modulus
      * 
-     * # if n is odd
+     * if we take n % 4 we how many numbers remained without adding it to a group
+     * 1, 2, 3, 4 => 4 % 4 = 0 No elements remained without adding to a group [ [1, 4] [2, 3] ]
+     * 1, 2, 3, 4, 5 => 5 % 4 = 1 There is one element remained which is 1 [ [2, 5] [3, 4] ] so the result should be 1
+     * 1, 2, 3, 4, 5, 6 => 6 % 4 = 2 There are two elements remained which are 3, 4 [ [1, 6] [2, 5] ] and the result should be 1 | 3 - 4 |
+     * 1, 2, 3, 4, 5, 6, 7, 8, 9
+     * 9 % 4 = 1
+     * [ [2 + 9, 4 + 7] [3 + 8, 5 + 6] ] and if we add 1 to either the first one or the second one the result of their difference will be 1
      * 
-     * let n be 7
-     * 1, 2, 3, 4, 5, 6, 7
-     * Remove the first element which is 1
-     * so the remained sequence is 2, 3, 4, 5, 6, 7 and their count is even
-     * so we can apply the above rule (even case) to the sequence
-     * and we might get either 1 or 0 as a result for the sequence without the first element we removed (which is 1)
-     * so we can take the absolute value of the difference between 1 and (1 or zero) and we will get the result
-     * 
-     * Solution Algorithm : 
-     * 
-     * if n is even
-     *  if n/2 is even the result is 0 (n, n-1, n-2, n-3 => | [n + n-3] - [n-1 + n-2] | = 0 )
-     *  if n/2 is odd the result will be 1 (n, n-1, n-2, n-3, n-4, n-5 => | n-2 - (n-3) | = 1)
-     * if n is odd
-     *  if n-1 is even
-     *      print 1 [1-0] (the sequence without 1 are two groups and their sum difference is 0)
-     *  if n-1 is odd
-     *      print 0 [1-1] (the sequence without 1 are two gropus and their sum difference is 1)
+     * Solution Algorithm :
+     * calculate n % 4
+     * if n is 0 or 3 so the result will be => 0
+     * else the result wil be 1
      **/
-
 
     ios_base::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
@@ -69,22 +61,9 @@ int main () {
     unsigned long long n;
     cin >> n;
 
-    if (n % 2 == 0) { // n is even
-        if ( (n/2) % 2 == 0 ) { // We can divide the sequence into to equal sets so the difference is 0
-            cout << 0;
-        }
-        else { // There are two numbers remained from the sequence and because they are continously their difference is 1
-            cout << 1;
-        }
-    }
-    else { // n is odd
-        if ( ((n-1) / 2) % 2 == 0 ) {
-            cout << 1; // 1 - 0
-        }
-        else {
-            cout << 0; // 1 - 1
-        }
-    }
+    n %= 4;
+
+    cout << ((n == 0 || n == 3) ? 0 : 1);
 
     return 0;
 }
